239536af567eedb48b6914aa92ea227da548c537
Kim Jin Hoon <dnvy0084@gmail.com>
2013-01-09 01:31:09 -0500

    process 객체 사용법

--- step-0-helloWorld/console-log.js
+++ step-0-helloWorld/console-log.js
@@ -25,3 +25,14 @@ for( var i = 1e4; i--; )
 }
 
 console.timeEnd( 'someLabel' );
+
+//console.log with colors;
+console.log( '\u001b[31m', 'hello world!' );
+console.log( '\u001b[32m', 'hello world!' );
+console.log( '\u001b[33m', 'hello world!' );
+console.log( '\u001b[34m', 'hello world!' );
+console.log( '\u001b[35m', 'hello world!' );
+console.log( '\u001b[36m', 'hello world!' );
+console.log( '\u001b[37m', 'hello world!' );
+console.log( '\u001b[0m', 'hello world!' );
+
