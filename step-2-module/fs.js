5dd5f7027aee1f597335632675b627477a202461
Kim Jin Hoon <dnvy0084@gmail.com>
2013-01-09 03:00:27 -0500

    file system 사용법

---
+++ step-2-module/fs.js
@@ -0,0 +1,32 @@
+var fs = require( 'fs' );
+
+//현재 실행중인 js파일이 있는 폴더 경로. cloud9환경이라 그런지 상대경로로 접근이 안됨. 
+var path = process.argv[1].replace( /\w+\.\w+/, '' );
+
+
+//동기식으로 파일 읽기
+
+try
+{
+    var text = fs.readFileSync( path + 'file/test.txt', 'utf-8' );
+    console.log( 'readFile Sync', text )//.toString() );
+}
+catch( e )
+{
+    console.log( e );
+}
+
+
+//비동기식 파일 읽기
+fs.readFile( path + 'file/test.txt', 'utf-8', function( error, data )
+{
+    console.log( 'readFile ASync', data );
+});
+
+//fs.writeFileSync( path + 'file/test.txt', text + ': written sync', 'utf8' );
+
+//비동기식 파일 쓰기
+fs.writeFile( path + 'file/test.txt', text + ': written async', function( error )
+{
+    console.log( 'write file async complete' );
+});
