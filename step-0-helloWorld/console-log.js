4ad10f42320aa50a4e1b6425e414c7b6bb6eb123
kim.jinhoon <dnvy0084@gmail.com>
2013-01-09 12:24:05 +0900

    Revert "console 객체 메소드 활용."
    This reverts commit 4f9992c07ba30cfde964e3bba3067c24b7b741c5.

--- step-0-helloWorld/console-log.js
+++ step-0-helloWorld/console-log.js
@@ -1,27 +1,2 @@
 //console.log();
 console.log( 'hello world' );
-
-//console.log with formatted;
-var a = 10;
-var b = 'String';
-var c = { a: 10, b: 'String', d: 30 };
-console.log( 'a=%d, b=%s, c=%j, d=%d', a, b, c, c.d );
-
-//console.info();
-console.info( 'console.info %s', 'same as console.log' );
-
-//console.error();
-console.error( 'some error' );
-
-//console.warn();
-console.warn( 'same as console.error' );
-
-//console.time( label );
-console.time( 'someLabel' );
-
-for( var i = 1e4; i--; )
-{
-    a += Math.sqrt( a );
-}
-
-console.timeEnd( 'someLabel' );
