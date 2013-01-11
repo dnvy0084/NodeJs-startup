fc971328009ad3a9a182682f4f2145b5b566a0df
Kim Jin Hoon <dnvy0084@gmail.com>
2013-01-10 21:14:05 -0500

    connect module basic

--- step-4-externalModule/connect.js
+++ step-4-externalModule/connect.js
@@ -1,3 +1,22 @@
-var http = require("http")
-  , fs = require("fs")
-  , url = require("url");
+var connect = require("connect");
+  
+var server = connect.createServer( function( req, res, next )
+{
+    //res.writeHead( 200, {'Content-Type': 'text/html'} );
+    //res.end( '<h1>hello connect module</h1>' );
+    console.log( 'call 1' );
+    next();
+    
+}, function( req, res, next )
+{
+    console.log( 'call 2' );
+    next();
+ 
+}, function( req, res, next )
+{
+    console.log( 'call 3' );
+    
+    res.writeHead( 200, {'Content-Type': 'text/html'} );
+    res.end( '<h1>hello connect module</h1>' );
+ 
+}).listen( process.env.PORT, process.env.IP );
